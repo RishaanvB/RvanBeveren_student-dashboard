795a83015b2f3adb7b877aed4ed6041eebe636be
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-01-23 16:19:54 +0100

    victory installed and minimum data showing in graph

--- student-db/src/App.js
+++ student-db/src/App.js
@@ -1,5 +1,36 @@
+import studentsData from "./mock-data/students.json";
+import { VictoryBar, VictoryChart, VictoryAxis } from "victory";
+
 function App() {
-  return <div>Dashboard dit gaat onmogelijk worden. Ziet er veel te moeilijk uit... :'P</div>;
+  const evelyn = studentsData
+    .filter((student) => student.name === "Evelyn")
+    .slice(0, 10);
+  console.log(evelyn);
+  return (
+    <div>
+      <h1>VictoryBar</h1>
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+          tickFormat={[
+            "wk1",
+            "wk2",
+            "wk3",
+            "wk4",
+            "wk5",
+            "wk6",
+            "wk7",
+            "wk8",
+          ]}
+        />
+        <VictoryAxis 
+        dependentAxis
+        tickValues={[1, 2, 3, 4, 5]}
+        tickFormat={(x)=> (`${x} pnt`)}/>
+        <VictoryBar data={evelyn} x={"assignment"} y={"difficulty-rating"} />
+      </VictoryChart>
+    </div>
+  );
 }
 
 export default App;
