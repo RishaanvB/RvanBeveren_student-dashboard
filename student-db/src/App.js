a20b93ee6f28c752f3ef3acb33a5589ecc8d23db
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-01-27 22:03:52 +0100

    changed avgRating to return array of objects

--- student-db/src/App.js
+++ student-db/src/App.js
@@ -6,25 +6,39 @@ function App() {
   //compare the value from that array with students.json
   //and then take the difficultyrating and push it to the 2nd index of the array
 
-  const getAvgRating = () => {
-    const filteredByAssignment = studentDB.map((student) => student.assignment);
+  const getAvgRating = (array, rating) => {
+    //this calculates average of value rating for key assignment of format  [{key: rating, assignment: value}, {object}, {object}]
+    //****kan gebruikt worden voor avg per student met extra parameter???
+
+    // filters by assignment and create non-duplicate array from assignment
+    const filteredByAssignment = array.map((student) => student.assignment);
+    // console.log(filteredByAssignment);
     const uniqueAssignments = [...new Set(filteredByAssignment)];
-    const sortedByAssignment = uniqueAssignments.map((assignment) => [
-      assignment,
-      studentDB
+    // map new Set and create numrating of corresponding assignment
+    const sortedByAssignment = uniqueAssignments.map((assignment) => {
+      const ratingList = array
         .filter((student) => student.assignment === assignment)
-        .map((student) => student["difficultyRating"]),
-    ]);
+        .map((student) => student[rating]);
+        console.log(ratingList);
+      return {
+        
+        assignment: assignment,
+        arrayOfRatings: ratingList,
+      };
+    });
+    //calc avg for arrayOfRating: ratingList  in previous aray[assignment, [1, 2, 3, 4]] = [assignment, avg(2.5)]
     const reducer = (acc, curr) => acc + curr;
-    const avgRating = sortedByAssignment.map((item) => [
-      item[0],
-      item[1].reduce(reducer) / item[1].length,
-    ]);
-    return avgRating
-    console.log(avgRating, "avg");
+    const avgRating = sortedByAssignment.map((item) => {
+     const avgNumber = item.arrayOfRatings.reduce(reducer)/ item.arrayOfRatings.length; //of gewoon delen door 10 :)
+      return {
+        assignment: item.assignment,
+        avgRating: avgNumber
+          
+      };
+    });
+    return avgRating;
   };
-  console.log(getAvgRating());
- 
+  console.log(getAvgRating(studentDB, "funRating"));
 
   return <div>App</div>;
 }
