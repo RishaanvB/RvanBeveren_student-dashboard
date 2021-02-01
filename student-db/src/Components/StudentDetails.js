3ab44148ac93eb9ca547b3207529cb10b3db168c
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-02-02 00:06:28 +0100

    removed allstudents.js, implemented avg ratings in studentjson file

--- student-db/src/Components/StudentDetails.js
+++ student-db/src/Components/StudentDetails.js
@@ -1,54 +1,33 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Student from "../Containers/Student";
-import studentDB from "../mock-data/students.json";
-import AllStudents from "./AllStudents";
+// import studentDB from "../mock-data/students.json";
+// import AllStudents from "./AllStudents";
 import RatingCheckboxForm from "./RatingCheckboxForm";
+import allStudentsData from "./helperFunctions";
 
 const StudentDetails = (props) => {
   const { name } = useParams();
+  console.log(name, "name in studentdetail");
+  // console.log(allStudentsData);
   // console.log(props, "rendering props in StudentDetails");
   // console.log("studentDetails rendered");
   // console.log(props, "props in studentDetails");
 
-  const getAvgRating = (array, rating) => {
-    //this calculates average of value rating for key assignment of format  [{key: rating, assignment: value}, {object}, {object}]
-    const filteredByAssignment = array.map((student) => student.assignment);
-    const uniqueAssignments = [...new Set(filteredByAssignment)];
-    const sortedByAssignment = uniqueAssignments.map((assignment) => {
-      const ratingList = array
-        .filter((student) => student.assignment === assignment)
-        .map((student) => student[rating]);
-      return {
-        assignment: assignment,
-        arrayOfRatings: ratingList,
-      };
-    });
-    const reducer = (acc, curr) => acc + curr;
-    const avgRating = sortedByAssignment.map((item) => {
-      const avgNumber =
-        item.arrayOfRatings.reduce(reducer) / item.arrayOfRatings.length;
-      return {
-        assignment: item.assignment,
-        avgRating: avgNumber,
-      };
-    });
-    return avgRating;
-  };
+  // console.log(allStudentsData, "allstudentdata");
 
   // console.log(getAvgRating(studentDB, "funRating"));
 
-  const singleStudentData = studentDB.filter(
+  const singleStudentData = allStudentsData.filter(
     (student) => student.name === name
   );
-  //   console.log(singleStudentData, `data of student ${name}`);
+  console.log(singleStudentData, `data of student ${name}`);
 
-  const [studentData, setStudentData] = useState("");
+  // const [studentData, setStudentData] = useState("");
   //   setStudentData(singleStudentData);
-  const [funData, setFunData] = useState(getAvgRating(studentDB, "funRating"));
-  const [difficultyData, setDifficultyData] = useState(
-    getAvgRating(studentDB, "difficultyRating")
-  );
+  // setStudentData(singleStudentData(allStudentsData));
+  // setStudentData(allStudentsData.filter(student=> student.name === name))
+  // console.log(studentData, "studentdata in studentdetails");
 
   const [showFunRating, setShowFunRating] = useState(true);
   const [showDifficultyRating, setShowDifficultyRating] = useState(true);
@@ -56,8 +35,8 @@ const StudentDetails = (props) => {
   const toggleShowFunRating = () => setShowFunRating((prev) => !prev);
   const toggleShowDifficultyRating = () =>
     setShowDifficultyRating((prev) => !prev);
-
-  console.log(showFunRating, "showRating");
+  // console.log(studentData, "studentData state");
+  // console.log(showFunRating, "showRating");
   return (
     <div>
       <h1>StudentDetails of {name} </h1>
@@ -69,21 +48,14 @@ const StudentDetails = (props) => {
         showFunRating={showFunRating}
       />
 
-      {name !== "All" ? (
-        <Student
-          studentName={name}
-          singleStudentData={singleStudentData}
-          showFunRating={showFunRating}
-          showDifficultyRating={showDifficultyRating}
-        />
-      ) : (
-        <AllStudents
-          funData={funData}
-          difficultyData={difficultyData}
-          showFunRating={showFunRating}
-          showDifficultyRating={showDifficultyRating}
-        />
-      )}
+      <Student
+        studentName={name}
+        singleStudentData={singleStudentData}
+        showFunRating={showFunRating}
+        showDifficultyRating={showDifficultyRating}
+      />
+
+      
     </div>
   );
 };
---
+++ student-db/src/Components/helperFunctions.js
@@ -0,0 +1,55 @@
+import studentDB from "../mock-data/students.json";
+
+const getAvgRating = (array, funRating, difficultyRating) => {
+  /*  creates new array of objects with avg rating of all students in format: [
+    {
+        "name": "All",
+        "assignment": "assignment",
+        "difficultyRating": avg,
+        "funRating": avg
+      }, {},{}... */
+  const filteredByAssignment = array.map((student) => student.assignment);
+  const uniqueAssignments = [...new Set(filteredByAssignment)];
+  //   console.log(uniqueAssignments);
+  const sortedByAssignment = uniqueAssignments.map((assignment) => {
+    const funRatingList = array
+      .filter((student) => student.assignment === assignment)
+      .map((student) => student[funRating]);
+    // console.log(funRatingList, "fun");
+
+    const difficultyRatingList = array
+      .filter((student) => student.assignment === assignment)
+      .map((student) => student[difficultyRating]);
+    // console.log(difficultyRatingList, "diff");
+    return {
+      assignment: assignment,
+      arrayDifficultyRating: difficultyRatingList,
+      arrayFunRating: funRatingList,
+    };
+  });
+
+  const reducer = (acc, curr) => acc + curr;
+  const avgRating = sortedByAssignment.map((item) => {
+    const avgFunNumber =
+      item.arrayFunRating.reduce(reducer) / item.arrayFunRating.length;
+    const avgDifficultyNumber =
+      item.arrayDifficultyRating.reduce(reducer) /
+      item.arrayDifficultyRating.length;
+    return {
+      name: "All",
+      assignment: item.assignment,
+      difficultyRating: avgDifficultyNumber,
+      funRating: avgFunNumber,
+    };
+  });
+  //   console.log(avgRating);
+  return avgRating;
+};
+
+const studentsAvg = getAvgRating(studentDB, "funRating", "difficultyRating");
+// console.log(studentsAvg, "studentsavg");
+
+const allStudentsData = studentDB.concat(studentsAvg);
+// console.log(allStudentsData, "studentData");
+
+export default allStudentsData;
